a5adc4b309cdd8075cf8b56c3f73eba5340cd91a
Kocheshkov Dmitrii <kocheshkovdmitrii@gmail.com>
2026-06-05 03:45:01 +0300

    Реализован solver Кочешкова

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -1,17 +1,18 @@
 public class Calculator {
     public int add(int a, int b){
-        //TODO inser your realisation in method add
+        return a + b;
     }
     public int dif(int a, int b){
-        //TODO inser your realisation in method dif
+        return a - b;
     }
     public int div(int a, int b){
-        //TODO inser your realisation in method div
+        if (b != 0) {return a / b;} else { return 0000;}
     }
     public int times(int a, int b){
-        //TODO inser your realisation in method times
+        return a * b;
     }
-    public int solver(){
-        //TODO inser your realisation in method solver
+    public int solver(int a, int b, int c){
+        int d = add(times(a, b), c);
+        return d;
     }
 }
