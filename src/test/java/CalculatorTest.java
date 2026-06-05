27a9c8ac269d75c0cbe0b617b5b17a0c128b5343
Kocheshkov Dmitrii <kocheshkovdmitrii@gmail.com>
2026-06-05 11:24:10 +0300

    Реализован solver Кочешкова Д. Д.

--- src/test/java/CalculatorTest.java
+++ src/test/java/CalculatorTest.java
@@ -1,31 +1,43 @@
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class CalculatorTest {
 
+    Calculator calculator = new Calculator();
+
     @Test
     void add() {
-        //TODO inser your realisation in test-method add
+        assertEquals(5, calculator.add(2, 3));
+        assertEquals(-1, calculator.add(2, -3));
+        assertEquals(0, calculator.add(0, 0));
     }
 
     @Test
     void dif() {
-        //TODO inser your realisation in test-method dif
+        assertEquals(2, calculator.dif(5, 3));
+        assertEquals(5, calculator.dif(2, -3));
+        assertEquals(0, calculator.dif(0, 0));
     }
 
     @Test
     void div() {
-        //TODO inser your realisation in test-method div
+        assertEquals(2, calculator.div(6, 3));
+        assertEquals(0, calculator.div(5, 0));
+        assertEquals(-2, calculator.div(6, -3));
     }
 
     @Test
     void times() {
-        //TODO inser your realisation in test-method times
+        assertEquals(6, calculator.times(2, 3));
+        assertEquals(-6, calculator.times(2, -3));
+        assertEquals(0, calculator.times(5, 0));
     }
 
     @Test
     void solver() {
-        //TODO inser your realisation in test-method solver
+        assertEquals(11, calculator.solver(2, 3, 5));
+        assertEquals(5, calculator.solver(0, 10, 5));
+        assertEquals(-1, calculator.solver(2, -3, 5));
     }
 }
